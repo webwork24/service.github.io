84b69ce7fe54bca2cd216121c85d4fa2b5a70ac7
webwork24 <31735275+webwork24@users.noreply.github.com>
2026-09-14 10:24:40 +0200

    Update script.js

--- locations/script.js
+++ locations/script.js
@@ -1,1 +1,9 @@
+import {IP} from '../constants.js';
 
+const urlParams = window.location.search;
+const urlRef = 'https://locations.' + IP;
+if (urlParams.length == 0) {
+    window.location.href = urlRef;
+} else {
+    window.location.href = urlRef + urlParams;
+}
